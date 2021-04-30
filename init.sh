21a4b21ffb574d3b84f87f8795c11ab620769a09
Justin Tung <justin.tung@live.ca>
2021-04-30 01:31:44 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 cd ~
 mkdir bin
 wget -c https://downloads.dockerslim.com/releases/1.35.1/dist_linux.tar.gz -O - | tar -xz -C ~/bin
-export PATH="$HOME/bin/dist_linux:$PATH"
+export PATH="$HOME/bin/dist_linux:$HOME/bin:$PATH"
 echo $PATH
